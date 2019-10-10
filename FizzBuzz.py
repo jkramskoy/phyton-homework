2907d3168772e9bc6e20dc4181b6a139777020b3
Julia Kramskoy <julia.kramskoy@gmail.com>
2019-10-10 13:41:49 -0500

    homework done

---
+++ FizzBuzz.py
@@ -0,0 +1,16 @@
+print("Hello.Please enter a number between 1 and 100")
+
+
+while (True):
+    number = int(input("Enter your number here"))
+    if number % 3 == 0 and number % 5 == 0:
+        print("fizzbuzz")
+    elif number % 5 == 0:
+        print("buzz")
+    elif number % 3 == 0:
+        print("fizz")
+
+
+
+
+
